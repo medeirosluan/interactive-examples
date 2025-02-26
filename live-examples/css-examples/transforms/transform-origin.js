843eee6675009eb90368e3230df45d68d2f17e81
Leo McArdle <leo@mca.is>
2025-02-26 17:34:26 +0000

    various fixes for css choice examples

--- live-examples/css-examples/transforms/transform-origin.js
+++ live-examples/css-examples/transforms/transform-origin.js
@@ -1,36 +1,26 @@
-'use strict';
-
-window.addEventListener('load', () => {
-  function update() {
-    const selected = document.querySelector('.selected');
-
-    /* Restart the animation
-           https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Tips */
-    el.className = '';
+function update(animation = "rotate") {
+  const el = document.getElementById("example-element");
+  /* Restart the animation
+          https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Tips */
+  el.className = "";
+  window.requestAnimationFrame(() => {
     window.requestAnimationFrame(() => {
-      window.requestAnimationFrame(() => {
-        el.className = selected.dataset.animation;
-      });
+      el.className = animation;
     });
+  });
 
-    const transformOrigin = getComputedStyle(el).transformOrigin;
-    const pos = transformOrigin.split(/\s+/);
-    crosshair.style.left = `calc(${pos[0]} - 12px)`;
-    crosshair.style.top = `calc(${pos[1]} - 12px)`;
+  const transformOrigin = getComputedStyle(el).transformOrigin;
+  const pos = transformOrigin.split(/\s+/);
+  crosshair.style.left = `calc(${pos[0]} - 12px)`;
+  crosshair.style.top = `calc(${pos[1]} - 12px)`;
+}
+
+const crosshair = document.getElementById("crosshair");
+update();
+crosshair.style.opacity = "1";
+
+window.addEventListener("message", ({ data }) => {
+  if (data.typ === "choice") {
+    update(data.code.includes("3D rotation") ? "rotate3d" : "rotate");
   }
-
-  const crosshair = document.getElementById('crosshair');
-  const el = document.getElementById('example-element');
-
-  const observer = new MutationObserver(() => {
-    update();
-  });
-
-  observer.observe(el, {
-    attributes: true,
-    attributeFilter: ['style'],
-  });
-
-  update();
-  crosshair.style.opacity = '1';
 });
